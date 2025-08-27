d7e61ca479bc7103cedea4cf3c56b71717dc852c
Fitness Tv <das07142@gmail.com>
2025-08-27 17:41:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { useReactToPrint } from "react-to-print";
+import ReactToPrint from "react-to-print";
 import {
   type InvoiceState,
   type InvoiceItem,
